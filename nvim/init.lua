68a49aa1e9f077d9ed12bb233ca740384b019ce9
hustavojhon <hustavojhon@gmail.com>
2024-03-24 13:05:16 -0500

    config basic for nvim with lua

---
+++ nvim/init.lua
@@ -0,0 +1,13 @@
+-- Configuration Basic
+-- C:\Users\$USER\AppData\Local\nvim
+
+vim.opt.syntax = 'enable'
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.wrap = true
+vim.opt.breakindent = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.background = 'dark'
