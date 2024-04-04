2b1f0cd8a98ece7fd94d98d14dfd554b2a01169d
hustavojhon <hustavojhon@gmail.com>
2024-04-03 23:22:51 -0500

    config nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,14 @@
 -- Configuration Basic
 -- C:\Users\$USER\AppData\Local\nvim
 
-vim.opt.syntax = 'enable'
-vim.opt.relativenumber = true
-vim.opt.number = true
-vim.opt.mouse = 'a'
-vim.opt.wrap = true
-vim.opt.breakindent = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.clipboard = 'unnamedplus'
-vim.opt.background = 'dark'
+-- +----------------------------+
+-- |     Import Lua modules     |
+-- +----------------------------+
+-- | 1. settings                |
+-- | 2. keymaps                 |
+-- | 3. plugins                 |
+-- +----------------------------+
+
+require('settings') 
+require('keymaps')
+require('plugins')
